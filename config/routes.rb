0a196c59306e5c3c528b0398aef45a3dcce4ad15
Jochen Roth <rothjochen@gmail.com>
2014-04-28 14:18:31 +0200

    seperate headline from info area

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-#get 'whitewall', :to => 'whitewall#index'
-
 get 'whitewall', :controller => 'index', :action => 'index'
 get 'whitewall_graph', :controller => 'graph', :action => 'index'
 get 'whitewall_graph', :controller => 'projects', :action => 'index'
