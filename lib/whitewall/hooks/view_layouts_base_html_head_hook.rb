d55bc43cbf1081ca11ad67ee355bc7e7af764ed0
ochorocho <rothjochen@gmail.com>
2018-05-05 01:13:28 +0200

    Add "set asignee as editor" button, limit drag&drop to issues only (fix)

--- lib/whitewall/hooks/view_layouts_base_html_head_hook.rb
+++ lib/whitewall/hooks/view_layouts_base_html_head_hook.rb
@@ -2,7 +2,7 @@ module Whitewall
   module Hooks
     class ViewLayoutsBaseHtmlHeadHook < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(context={})
-		  content = stylesheet_link_tag('application.css', :plugin => 'whitewall')
+		  content = stylesheet_link_tag('app.css', :plugin => 'whitewall') + javascript_include_tag('app.js', :plugin => 'whitewall')
       end
     end
   end
