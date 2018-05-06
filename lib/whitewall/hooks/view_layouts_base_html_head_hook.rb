801113f40459fe7d4e40764854196ef5ce285f8e
ochorocho <rothjochen@gmail.com>
2018-05-07 00:13:25 +0200

    Include css and js globally

--- lib/whitewall/hooks/view_layouts_base_html_head_hook.rb
+++ lib/whitewall/hooks/view_layouts_base_html_head_hook.rb
@@ -2,7 +2,12 @@ module Whitewall
   module Hooks
     class ViewLayoutsBaseHtmlHeadHook < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(context={})
-		  content = stylesheet_link_tag('app.css', :plugin => 'whitewall') + javascript_include_tag('app.js', :plugin => 'whitewall')
+
+        content = stylesheet_link_tag('app.css', :plugin => 'whitewall')
+        content += javascript_include_tag 'jquery.ba-throttle-debounce', :plugin => 'whitewall'
+        content += javascript_include_tag 'jquery.stickyheader', :plugin => 'whitewall'
+        content += javascript_include_tag 'app', :plugin => 'whitewall'
+
       end
     end
   end
